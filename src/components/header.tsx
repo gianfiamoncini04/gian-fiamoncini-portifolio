38c3e856f4ce62d2e55d03865518cf2117f6c652
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2023-09-12 16:35:10 -0300

    testando deno dev

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
    return (
             <>
               <section>
-                  <p>Header</p>
+                  <p>NO HEAEDer</p>
               </section>
             </>
        )

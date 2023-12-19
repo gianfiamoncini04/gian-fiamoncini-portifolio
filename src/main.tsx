1fe6ca3975a0d1a76e86eb2f6d55bc481b4b0151
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2023-12-19 09:31:20 -0300

    email working

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "@/constants/theme-provider"
+import { ThemeProvider } from "@/constants/theme-provider";
 import { I18nextProvider } from "react-i18next";
-import i18next from "i18next"
-import pt from '@/lib/languages/pt.json';
-import en from '@/lib/languages/en.json';
+import i18next from "i18next";
+import pt from "@/lib/languages/pt.json";
+import en from "@/lib/languages/en.json";
 
 i18next.init({
-    interpolation: { escapeValue: false },  
-    lng: 'pt',                              
-    resources: {
-        pt: {
-			common: pt
-        },
-        en: {
-			common: en    
-        },
-    },
+	interpolation: { escapeValue: false },
+	lng: "pt",
+	resources: {
+		pt: {
+			common: pt,
+		},
+		en: {
+			common: en,
+		},
+	},
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ThemeProvider storageKey="vite-ui-theme" defaultTheme="system">
 			<I18nextProvider i18n={i18next}>
-				<App />
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
 			</I18nextProvider>
 		</ThemeProvider>
 	</React.StrictMode>

34cad0c8e2e67513a27a64ffd9781f8ae06efc58
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2023-12-19 10:43:57 -0300

    fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ i18next.init({
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<ThemeProvider storageKey="vite-ui-theme" defaultTheme="system">
 		<I18nextProvider i18n={i18next}>
-			<BrowserRouter basename="/">
+			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</I18nextProvider>

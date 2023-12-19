65c8389fa1434f51fdc7ae3621d2b49796ffd67f
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2023-12-19 09:42:02 -0300

    removing react typed

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,4 +1,3 @@
-import Typed from "react-typed";
 import { useTranslation } from "react-i18next";
 
 export default function Body() {
@@ -7,13 +6,7 @@ export default function Body() {
 	return (
 		<>
 			<span className="text-4xl drop-shadow-[0_0_10px_#00000070] dark:drop-shadow-[0_0_10px_#FFFFFF60] dark:text-white font-bold">
-				Portifolio
-				<Typed
-					strings={[`\n${t("title")}`]}
-					typeSpeed={100}
-					backSpeed={100}
-					loop
-				/>
+				Portifolio {t('title')}
 			</span>
             <a href="/form">Form</a>
 		</>
